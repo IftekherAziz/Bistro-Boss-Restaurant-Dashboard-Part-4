3d7ed5c295eff6a45ba60f6fce88169ee99eb915
Iftekher Aziz <azizchy02@gmail.com>
2023-05-28 20:00:59 +0600

    React Query and load data

--- bistro-boss-client-with-auth-dashboard/src/hooks/useCart.jsx
+++ bistro-boss-client-with-auth-dashboard/src/hooks/useCart.jsx
@@ -1,12 +1,20 @@
+import { useQuery } from "@tanstack/react-query";
+import { useContext } from "react";
+import { AuthContext } from "../providers/AuthProviders";
 
+const useCart = () => {
+  const { user } = useContext(AuthContext);
 
-const useCart = (email) => {
-    
-    return (
-        <div>
-            
-        </div>
-    );
+  const { isLoading, data: cart = [] } = useQuery({
+    queryKey: ["carts", user?.email],
+    queryFn: async () => {
+      const res = await fetch(
+        `http://localhost:5000/carts?email=${user?.email}`
+      );
+      return res.json();
+    },
+  });
+  return { isLoading, cart };
 };
 
 export default useCart;
