d6558ca676b1fa546ee4a20fc1d897ba5005b4b0
Iftekher Aziz <azizchy02@gmail.com>
2023-05-28 20:54:38 +0600

    Create Dashboard route and Dashboard navigation

--- bistro-boss-client-with-auth-dashboard/src/Routes/Routes.jsx
+++ bistro-boss-client-with-auth-dashboard/src/Routes/Routes.jsx
@@ -8,6 +8,9 @@ import Error from "../pages/Error/Error";
 import SignUp from "../pages/SignUp/SignUp";
 import PrivateRoute from "./PrivateRoute";
 import Secret from "../pages/Shared/Secret/Secret";
+import Dashboard from "../Layout/Dashboard";
+import MyCart from "../pages/Dashboard/MyCart/MyCart";
+
 
 export const router = createBrowserRouter([
   {
@@ -21,26 +24,38 @@ export const router = createBrowserRouter([
       },
       {
         path: "/menu",
-        element:<Menu></Menu>
+        element: <Menu></Menu>,
       },
       {
         path: "/order/:category",
-        element: <Order></Order>
+        element: <Order></Order>,
       },
       {
         path: "/login",
-        element: <Login></Login>
+        element: <Login></Login>,
       },
       {
         path: "/signup",
-        element: <SignUp></SignUp>
+        element: <SignUp></SignUp>,
       },
       {
         path: "/secret",
-        element: <PrivateRoute>
-          <Secret></Secret>
-        </PrivateRoute>
-      }
+        element: (
+          <PrivateRoute>
+            <Secret></Secret>
+          </PrivateRoute>
+        ),
+      },
+    ],
+  },
+  {
+    path: "dashboard",
+    element: <Dashboard></Dashboard>,
+    children: [
+      {
+        path: "mycart",
+        element: <MyCart></MyCart>,
+      },
     ],
   },
 ]);
