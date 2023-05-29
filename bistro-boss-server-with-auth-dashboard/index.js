4fc4ecfd0057f499f2f0f260bc4b8a909ae95d30
Iftekher Aziz <azizchy02@gmail.com>
2023-05-29 20:32:17 +0600

    Display cart items to review before order

--- bistro-boss-server-with-auth-dashboard/index.js
+++ bistro-boss-server-with-auth-dashboard/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -63,12 +63,12 @@ async function run() {
         })
 
         // DELETE api/carts
-        // app.delete('/carts/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     const query = { _id: new ObjectId(id) };
-        //     const result = await cartCollection.deleteOne(query);
-        //     res.send(result);
-        // })
+        app.delete('/carts/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await cartCollection.deleteOne(query);
+            res.send(result);
+        })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
